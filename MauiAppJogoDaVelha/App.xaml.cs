383fb4c54aef6350a6a2569475c444cf7470a747
CarinaMendes <190530193+CarinaMendesDev@users.noreply.github.com>
2025-05-11 20:16:50 -0300

    Update App.xaml.cs
    ajuste de largura e altura

--- MauiAppJogoDaVelha/App.xaml.cs
+++ MauiAppJogoDaVelha/App.xaml.cs
@@ -2,11 +2,15 @@
 {
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
-
-            MainPage = new AppShell();
-        }
-    }
-}
+ InitializeComponent();
+ MainPage = new AppShell();
+ }
+ protected override Window CreateWindow(IActivationState activationState)
+ {
+ var window = base.CreateWindow(activationState);
+ window.Width = 400;
+ window.Height = 600;
+ return window;
+ }
+} // Fecha classe
+} // Fecha namespace
